79edf7e9b19b6b594a197b88e95c0a011d4def6d
TzuYing <musigm5456@gmail.com>
2023-09-21 16:30:10 +0800

    chore(api): [Client] overwrite request

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -3,6 +3,7 @@
 namespace OnrampLab\ElevenlabsApiClient\Api;
 
 use OnrampLab\ApiClient\Api\Client as ApiClient;
+use Psr\Http\Message\ResponseInterface;
 
 class Client extends ApiClient
 {
@@ -20,5 +21,20 @@ class Client extends ApiClient
         $payload['headers']['xi-api-key'] = $this->apiToken;
 
         return $payload;
+    }
+
+    public function request(string $method, string $url, array $params = [], array $data = []): ResponseInterface
+    {
+        $accept = strpos($url, 'https://api.elevenlabs.io/v1/text-to-speech') === 0 ? 'audio/mpeg' : 'application/json';
+
+        $payload = $this->applyMiddlewares([
+            'query' => $params,
+            'json' => (object) $data,
+            'headers' => [
+                'Accept' => $accept,
+            ],
+        ]);
+
+        return $this->httpClient->request($method, $url, $payload);
     }
 }
