a5d32f462d92a3a980046835b67cb56c1926cf21
TzuYing <musigm5456@gmail.com>
2023-09-21 16:29:10 +0800

    chore(api): [Client] update base URL and api token

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -9,8 +9,16 @@ class Client extends ApiClient
 
     public function __construct($config)
     {
-        $config['baseUrl'] = 'https://www.example.com';
+        $config['baseUrl'] = 'https://api.elevenlabs.io';
+
         parent::__construct($config);
 
+    }
+
+    public function applyAuth(array $payload): array
+    {
+        $payload['headers']['xi-api-key'] = $this->apiToken;
+
+        return $payload;
     }
 }
