a30015989bff5478c699ff48e409fb536fda38b8
TzuYing <musigm5456@gmail.com>
2023-09-21 16:19:21 +0800

    refactor(api): [SampleResource] remove unused resource

--- src/Api/Client.php
+++ src/Api/Client.php
@@ -3,17 +3,14 @@
 namespace OnrampLab\ElevenlabsApiClient\Api;
 
 use OnrampLab\ApiClient\Api\Client as ApiClient;
-use OnrampLab\ElevenlabsApiClient\Api\Resources\SampleResource;
 
 class Client extends ApiClient
 {
-    public SampleResource $sampleResource;
 
     public function __construct($config)
     {
         $config['baseUrl'] = 'https://www.example.com';
         parent::__construct($config);
 
-        $this->sampleResource = new SampleResource($this);
     }
 }
